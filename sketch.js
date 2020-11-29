f547f183a8ba823a58a03b947cc11df696e78e67
Varun44445 <72554382+Varun44445@users.noreply.github.com>
2020-11-29 12:02:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ var bullet, speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
   speed=random(223,321);
   weight=random(30,52);
   thickness=random(22,83);
